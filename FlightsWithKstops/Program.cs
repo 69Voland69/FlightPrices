05e092edb69574b28e1ded9cfed0b30fff562e5e
69Vol <69vOLAND69@GMAIL.COM>
2024-10-23 23:05:21 +0300

    Final Solution

--- FlightsWithKstops/Program.cs
+++ FlightsWithKstops/Program.cs
@@ -11,140 +11,40 @@ int[][] flights4 =
     [9, 7, 3], [7, 0, 5], [6, 5, 8], [1, 7, 6], [4, 0, 9], [5, 9, 1], [8, 7, 3], [1, 2, 6], [4, 1, 5], [5, 2, 4],
     [1, 9, 1], [7, 8, 10], [0, 4, 2], [7, 2, 8]
 ];
-
+int[][] flights5 = [[2, 3, 200],  [1, 2, 100], [2, 0, 100], [1,3,600], [0, 1, 100]];
 var sln = new Solution();
 
+int[][] fl6 = [[0,1,20],[1,2,20],[2,3,20],[3,4,20],[4,5,20],[5,6,20],[6,7,20],[7,8,20],[8,9,20],[0,2,9999],[2,4,9998],[4,7,9997]];
 var chp = sln.FindCheapestPrice(4,flights1, 0,3,1);
 var chp1 = sln.FindCheapestPrice(3, flights2, 0, 2, 1);
 var chp2 = sln.FindCheapestPrice(3, flights3, 0, 2, 0);
 var chp3 = sln.FindCheapestPrice(10, flights4, 6, 0, 7);
+var chp4 = sln.FindCheapestPrice(10,fl6, 0,9,4);
+var a = 3;
 
-var a = chp;
 
-public class Solution
+
+
+public class Sl2
 {
-    public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
-    {
-        var a = GetGraph(flights, src, out Dictionary<int,Node> nodes);
-        SetPrices(a, 0, 0, src);
+    public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] distances = new int[n];
+        Array.Fill(distances, int.MaxValue);
+        distances[src] = 0;
 
-        var ds = nodes[dst];
-        var bestPrice = ds.HopsAndPrices.Where(x => x.Key <= k).Select(x => x.Value).OrderBy(x=>x).FirstOrDefault();
-        
-        return bestPrice == default(int) ? -1 : bestPrice;
-    }
-
-    public Node GetGraph(int[][] flights, int src, out Dictionary<int,Node> nodes)
-    {
-        nodes = new Dictionary<int, Node>();
-
-        foreach (var flight in flights)
-        {
-            nodes.TryGetValue(flight[0], out var srcNode);
-
-            if (srcNode == null)
-            {
-                srcNode = new Node() { NodeId = flight[0] };
-                nodes.Add(flight[0], srcNode);
-            }
-
-            var destination = flight[1];
-            var price = flight[2];
-
-            nodes.TryGetValue(destination, out var destNode);
-
-            if (destNode == null)
-            {
-                destNode = new Node() { NodeId = destination };
-                nodes.Add(destination, destNode);
-
-            }
-
-            var newLink = new Link()
-            {
-                NodeFrom = srcNode,
-                NodeTo = destNode,
-                Price = price
-            };
-
-            srcNode.Children.Add(newLink);
-        }
-
-        return nodes[src];
-
-    }
-
-    public void SetPrices(Node node, int stop, int priceToGet, int from)
-    {
-        foreach (var link in node.Children)
-        {
-            if (link.NodeTo.CheckedBy.Contains(node.NodeId))
-            {
-                continue;
-            }
+        for (int i = 0; i <= k; ++i) {
+            int[] temp = (int[])distances.Clone();
             
-            if (!link.NodeTo.HopsAndPrices.TryGetValue(stop, out var price))
-            {
-                link.NodeTo.HopsAndPrices.Add(stop, link.Price + priceToGet);    
+            foreach (var flight in flights) {
+                int from = flight[0], to = flight[1], cost = flight[2];
+                if (distances[from] != int.MaxValue && distances[from] + cost < temp[to]) {
+                    temp[to] = distances[from] + cost;
+                }
             }
-            else if(price > priceToGet + link.Price)
-            {
-                link.NodeTo.HopsAndPrices[stop] = priceToGet + link.Price;
-            }
+
+            distances = temp;
         }
 
-        node.CheckedBy.Add(from); 
-
-        foreach (var link in node.Children.Where(x=>!x.NodeTo.CheckedBy.Contains(node.NodeId)))
-        {
-            SetPrices(link.NodeTo, stop+1, link.Price, node.NodeId);
-        }
-
+        return distances[dst] == int.MaxValue ? -1 : distances[dst];
     }
-    
-    
-    public class Node
-    {
-        public Node()
-        {
-            HopsAndPrices = new Dictionary<int, int>();
-            Children = new List<Link>();
-            Parent = new List<Link>();
-            CheckedBy = new HashSet<int>();
-        }
-
-        public HashSet<int> CheckedBy { get; set; }
-
-        public int NodeId { get; set; }
-        
-        public Dictionary<int, int> HopsAndPrices { get; }
-        
-        public List<Link> Children { get; set; } 
-        
-        public List<Link> Parent { get; set; }
-
-        public override string ToString()
-        {
-            return $"{NodeId}";
-        }
-    }
-
-    public class Link
-    {
-        public Link()
-        {
-        }
-
-        public Node NodeFrom { get; set; }
-        
-        public Node NodeTo { get; set; }
-        
-        public int Price { get; set; }
-
-        public override string ToString()
-        {
-            return $"From: {NodeFrom.NodeId} To: {NodeTo.NodeId} Price: {Price}";
-        }
-    }
-}   
-    
+}
---
+++ FlightsWithKstops/Solution.cs
@@ -0,0 +1,130 @@
+﻿public class Solution
+{
+    public int FindCheapestPrice(int n, int[][] flights, int src, int dst, int k)
+    {
+        var nodes = GetGraph(flights,k);
+
+        if (!nodes.TryGetValue(dst, out _) || !nodes.TryGetValue(src, out _))
+        {
+            return -1;
+        }
+        
+        Array.Fill(nodes[src].Price.Value,0);
+        SetPrices(nodes[src], k);
+
+        var ds = nodes[dst];
+
+        return ds.Price.Value.Min() == Int32.MaxValue ? -1 : ds.Price.Value.Min();
+    }
+
+    public Dictionary<int,Node> GetGraph(int[][] flights, int k)
+    {
+        var nodes = new Dictionary<int, Node>();
+
+        foreach (var flight in flights)
+        {
+            nodes.TryGetValue(flight[0], out var srcNode);
+
+            if (srcNode == null)
+            {
+                srcNode = new Node(k) { NodeId = flight[0] };
+                nodes.Add(flight[0], srcNode);
+            }
+
+            var destination = flight[1];
+            var price = flight[2];
+
+            nodes.TryGetValue(destination, out var destNode);
+
+            if (destNode == null)
+            {
+                destNode = new Node(k) { NodeId = destination };
+                nodes.Add(destination, destNode);
+
+            }
+
+            var newLink = new Link()
+            {
+                NodeTo = destNode,
+                Price = price
+            };
+
+            srcNode.Children.Add(newLink);
+        }
+
+        return nodes;
+
+    }
+
+    public void SetPrices(Node nodeStart, int k)
+    {
+        var nodes = new Queue<Node>();
+        
+        nodes.Enqueue(nodeStart);
+
+        for (int i = 0; i <= k; i++)
+        {
+            
+            var newQueue = new Queue<Node>();
+            
+            while(nodes.TryDequeue(out var node))
+            {
+                foreach (var link in node.Children)
+                {
+                    var price = node.Price.Value[(i-1) < 0 ? 0 : i-1] + link.Price;
+                    
+                    if (link.NodeTo.Price.Value[i] < price)
+                    {
+                        continue;
+                    }
+
+                    link.NodeTo.Price.Value[i] = price;
+                    newQueue.Enqueue(link.NodeTo);
+                }
+            }
+
+            nodes = newQueue;
+        }
+    }
+    
+    
+    public class Node
+    {
+        public Node(int k)
+        {
+            Children = new List<Link>();
+            Price = new Lazy<int[]>(() =>
+            {
+                var a = new int[k+1];
+                Array.Fill(a, int.MaxValue);
+                return a;
+            });
+           
+        }
+
+
+        public int NodeId { get; set; }
+
+        public Lazy<int[]> Price { get; set; }
+        
+        public List<Link> Children { get; set; } 
+        
+        public override string ToString()
+        {
+            return $"{NodeId}";
+        }
+    }
+
+    public class Link
+    {
+        
+        public Node NodeTo { get; set; }
+        
+        public int Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"To: {NodeTo.NodeId} Price: {Price}";
+        }
+    }
+}
